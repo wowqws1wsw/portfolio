f31b2cda248b872f1a5314c79b5016438c67dc4c
arfannaqi <muhammadarfannaqi@smkwikrama.sch.id>
2023-12-12 14:17:11 +0700

    revisi

--- js/script.js
+++
@@ -1,6 +0,0 @@
-//toggle class active
-const navbarNav = document.querySelector
-('.navbar-nav,navbar-extra ')
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.className.toggle('active');
-};
